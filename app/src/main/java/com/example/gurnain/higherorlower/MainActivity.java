974e96fa9b5da5d8c9bca735b3cf31e2ee3f2d9f
Gurnain <gurnaindeepsingh@gmail.com>
2018-01-10 20:25:33 -0500

    Updated the file

--- app/src/main/java/com/example/gurnain/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/gurnain/higherorlower/MainActivity.java
@@ -43,13 +43,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rand = r.nextInt(20)+1;
-
-        /*Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                higherLower(rand);
-            }
-        });*/
     }
 }
